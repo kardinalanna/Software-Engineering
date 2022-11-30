83e257af973e1aa7838c4574ea4c425fe6c65b55
Anna Karandina <anutajjk@yandex.ru>
2022-11-30 16:56:46 +0300

    #4 parser and bot work correctly

--- main.py
+++ main.py
@@ -3,7 +3,28 @@ from aiogram import Bot, types, utils
 from aiogram.dispatcher import Dispatcher
 from aiogram.utils import executor
 
+def searcher(text):
+    res = YoutubeSearch(text, max_results=10).to_dict()
+    return res
+    
 bot = Bot(token=TOKEN)
 dp = Dispatcher(bot)
 
+
+@dp.inline_handler() #работает в inline режиме
+async def inline_handler(query: types.InlineQuery):
+    text = query.query or 'echo' #сюда попадает запрос
+    links = searcher(text) #запрос отправляем на парсинг
+
+    articles = [types.InlineQueryResultArticle( #окно с результатами парсинга
+        id=hashlib.md5(f'{link["id"]}'.encode()).hexdigest(),
+        title=f'{link["title"]}',
+        url=f'https://www.youtube.com/watch?v={link["id"]}',
+        thumb_url=f'{link["thumbnails"][0]}',
+        input_message_content=types.InputTextMessageContent(
+            message_text=f'https://www.youtube.com/watch?v={link["id"]}'
+        )
+    )for link in links]
+
+    await query.answer(articles, cache_time=60, is_personal=True) # отправляем ссылки по нажатию
 executor.start_polling(dp, skip_updates=True)
